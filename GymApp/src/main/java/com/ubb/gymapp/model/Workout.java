f29f318aa76cf482526a2b30a3c91ca48668e5a1
Triff Petra <Triff Petra@Petra>
2017-10-24 16:24:18 +0300

    Workout CRUD wip

--- GymApp/src/main/java/com/ubb/gymapp/model/Workout.java
+++ GymApp/src/main/java/com/ubb/gymapp/model/Workout.java
@@ -1,6 +1,19 @@
 package com.ubb.gymapp.model;
 
-public class Workout {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "workout")
+@SequenceGenerator (sequenceName = "workout_seq", allocationSize =1, name = "workoutSequence")
+public class Workout implements Serializable{
 
 	private Integer workoutId;
 	private String workoutType;
@@ -18,6 +31,9 @@ public class Workout {
 		this.description = description;
 	}
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "workoutSequence")
+	@Column (name = "idWorkout", unique = true, nullable = false)
 	public Integer getWorkoutId() {
 		return workoutId;
 	}
@@ -26,6 +42,7 @@ public class Workout {
 		this.workoutId = workoutId;
 	}
 
+	@Column (name = "WorkoutType")
 	public String getWorkoutType() {
 		return workoutType;
 	}
@@ -34,6 +51,7 @@ public class Workout {
 		this.workoutType = workoutType;
 	}
 
+	@Column (name = "Difficulty")
 	public String getDifficulty() {
 		return difficulty;
 	}
@@ -42,12 +60,56 @@ public class Workout {
 		this.difficulty = difficulty.toString();
 	}
 
+	@Column (name = "Desc")
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((difficulty == null) ? 0 : difficulty.hashCode());
+		result = prime * result + ((workoutId == null) ? 0 : workoutId.hashCode());
+		result = prime * result + ((workoutType == null) ? 0 : workoutType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Workout other = (Workout) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (difficulty == null) {
+			if (other.difficulty != null)
+				return false;
+		} else if (!difficulty.equals(other.difficulty))
+			return false;
+		if (workoutId == null) {
+			if (other.workoutId != null)
+				return false;
+		} else if (!workoutId.equals(other.workoutId))
+			return false;
+		if (workoutType == null) {
+			if (other.workoutType != null)
+				return false;
+		} else if (!workoutType.equals(other.workoutType))
+			return false;
+		return true;
 	}
 
 }
