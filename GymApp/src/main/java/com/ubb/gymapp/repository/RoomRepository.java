241d66cd475179459a91abe1c4e76ac5e39cd1a5
Pirpi <Pirpi@172.30.116.226>
2017-10-24 16:56:48 +0300

    Find Room by name

--- GymApp/src/main/java/com/ubb/gymapp/repository/RoomRepository.java
+++ GymApp/src/main/java/com/ubb/gymapp/repository/RoomRepository.java
@@ -1,9 +1,11 @@
 package com.ubb.gymapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.ubb.gymapp.model.Room;
 
 public interface RoomRepository extends CrudRepository<Room,Long> {
-
+	public List<Room> findRoomByName(String name);
 }
