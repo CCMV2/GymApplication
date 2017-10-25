0da942c84b2135ae3acd712509de1c27eaf7bd49
Beaa <Beaa@Beatrice>
2017-10-25 10:36:47 +0300

    cgsdf

--- GymApp/src/main/java/com/ubb/gymapp/repository/TimetableRepository.java
+++ GymApp/src/main/java/com/ubb/gymapp/repository/TimetableRepository.java
@@ -1,9 +1,12 @@
 package com.ubb.gymapp.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ubb.gymapp.model.Timetable;
 
-public interface TimetableRepository extends CrudRepository<Timetable, Long>{
+public interface TimetableRepository extends JpaRepository<Timetable, Long>{
 
 }
+
+
+
