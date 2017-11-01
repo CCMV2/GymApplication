8b1667d71338bac1139cc51dca656e500430e939
Ganditorul <robert_eu_97@yahoo.com>
2017-11-01 11:57:26 +0200

    Timetable again

--- GymApp/src/main/java/com/ubb/gymapp/repository/TimetableRepository.java
+++ GymApp/src/main/java/com/ubb/gymapp/repository/TimetableRepository.java
@@ -1,29 +1,18 @@
 package com.ubb.gymapp.repository;
 
-<<<<<<< HEAD
 import java.util.List;
 
-=======
->>>>>>> branch 'develop' of https://github.com/ProiectColectivIG3/GymApplication.git
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ubb.gymapp.model.Timetable;
 
 public interface TimetableRepository extends JpaRepository<Timetable, Long>{
-<<<<<<< HEAD
 	
 	List<Timetable> findByDay(String day);
 	List<Timetable> findByidTimetable(int id);
 	List<Timetable> findByidWorkout(int id);
-	List<Timetable> findByroom_idroom(int id);
+	List<Timetable> findByRoom(int id);
 	
 	void deleteInTimetable(List<Timetable> entities);
 	
 }
-=======
-
-}
-
-
-
->>>>>>> branch 'develop' of https://github.com/ProiectColectivIG3/GymApplication.git
