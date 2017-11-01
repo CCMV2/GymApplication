c99e0a046e76f96348fe3dfdb492e83bbc894acc
DESKTOP-98GU0VT\stefi <oltean.stefan96@gmail.com>
2017-11-01 10:46:20 +0200

    POM

--- GymApp/src/main/java/com/ubb/gymapp/init/WebAppConfig.java
+++ GymApp/src/main/java/com/ubb/gymapp/init/WebAppConfig.java
@@ -5,8 +5,9 @@ import java.util.Properties;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan; 	
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -52,6 +53,7 @@ public class WebAppConfig {
    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
       LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
       entityManagerFactoryBean.setDataSource(dataSource());
+      entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
       entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
       
       entityManagerFactoryBean.setJpaProperties(hibProperties());
