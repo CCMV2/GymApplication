b9690a903eb4ec4063cc86f125d35a25b91c8aa8
Gellert <Gellert@192.168.0.89>
2017-11-02 21:46:54 +0200

    UserTest

--- GymApp/src/test/java/com/ubb/gymapp/GymappApplicationTests.java
+++ GymApp/src/test/java/com/ubb/gymapp/GymappApplicationTests.java
@@ -1,5 +1,8 @@
 package com.ubb.gymapp;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.util.List;
 
 import org.junit.Test;
@@ -10,8 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ubb.gymapp.model.User;
 import com.ubb.gymapp.repository.UserRepository;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,6 +29,35 @@ public class GymappApplicationTests {
 		//assertNotNull(lista);
 		//assertNotEquals(lista.size(), 0);
 	}
+	
+	@Test
+	public void deleteUser(){
+		User newUser = new User(1L, "aaa", "aaa", "aaa", "aaa", 2L, "aaa", null);
+		userRepo.save(newUser);
+		User user =userRepo.findOne(newUser.getId());
+		userRepo.delete(user.getId());
+		assertNull(user.getId());
+	}
+	
+	@Test
+	public void testFindUserById(){
+		User user = userRepo.findOne((long) 1);
+		assertNotNull(user);
+	}
+	
+	@Test
+	public void testGetAllUsers(){
+		List<User> listOfUsers = userRepo.findAll();
+		assertNotNull(listOfUsers);
+	}
+	
+	@Test
+	public void testUpdateUsers(){
+		
+	}
+	
+	
+	
 	
 	
 
