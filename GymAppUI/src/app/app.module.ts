442e93c3f2cbab9002aaa9c6c693d5ffac09eb4a
Petra <flowers_petra@yahoo.com>
2017-11-06 18:31:27 +0200

    generated component create-workout

--- GymAppUI/src/app/app.module.ts
+++ GymAppUI/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateWorkoutComponent } from './create-workout/create-workout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateWorkoutComponent
   ],
   imports: [
     BrowserModule
---
+++ GymAppUI/src/app/create-workout/create-workout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-workout',
+  templateUrl: './create-workout.component.html',
+  styleUrls: ['./create-workout.component.css']
+})
+export class CreateWorkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
