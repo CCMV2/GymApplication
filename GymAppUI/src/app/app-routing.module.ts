db28c406481151f38cc0f27ff8cda6fe6196e670
reizmelinda <rmig0076@scs.ubbcluj.ro>
2017-11-06 20:14:49 +0200

    Added create-subscription component

--- GymAppUI/src/app/app-routing.module.ts
+++ GymAppUI/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CreateWorkoutComponent } from './create-workout/create-workout.component';
 import { UpdateSubscriptionComponent } from './update-subscription/update-subscription.component';
+import { CreateSubscriptionComponent } from './create-subscription/create-subscription.component';
 
 const routes: Routes = [
     //{ path: '', redirectTo: '/createworkout', pathMatch: 'full' },
     { path: 'createworkout', component: CreateWorkoutComponent },
-    { path: 'updatesubscription', component: UpdateSubscriptionComponent }
+    { path: 'updatesubscription', component: UpdateSubscriptionComponent },
+    { path: 'createsubscription', component: CreateSubscriptionComponent }
 ];
 
 @NgModule({
