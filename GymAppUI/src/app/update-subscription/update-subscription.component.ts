0a7c555ea1ebc6661e25983ee6aace6b2161394d
Alex Salajan <Alex@Alexs-MacBook-Pro-2.local>
2017-11-06 18:37:50 +0200

    generated update subscription component

---
+++ GymAppUI/src/app/update-subscription/update-subscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-subscription',
+  templateUrl: './update-subscription.component.html',
+  styleUrls: ['./update-subscription.component.css']
+})
+export class UpdateSubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
