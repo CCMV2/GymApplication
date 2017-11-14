2ebf033aab6347f5711a01486b776d76c262b7e0
Alex Salajan <Alex@Alexs-MacBook-Pro-2.local>
2017-11-14 18:27:39 +0200

    update routing

--- GymAppUI/src/app/app-routing.module.ts
+++ GymAppUI/src/app/app-routing.module.ts
@@ -8,7 +8,11 @@ import { ListWorkoutComponent } from './list-workout/list-workout.component';
 import { CreateSubscriptionComponent } from './create-subscription/create-subscription.component';
 import { ListSubscriptionComponent } from './list-subscription/list-subscription.component';
 import { RoomPageComponent } from './room-page/room-page.component';
-import {TimetableComponent} from './timetable/timetable.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
 
 const routes: Routes = [
     //{ path: '', redirectTo: '/createworkout', pathMatch: 'full' },
@@ -19,7 +23,11 @@ const routes: Routes = [
     { path: 'createsubscription', component: CreateSubscriptionComponent },
     { path: 'listsubscription', component: ListSubscriptionComponent },
     { path: 'roompage', component: RoomPageComponent },
-    { path: 'timetable', component: TimetableComponent }
+    { path: 'timetable', component: TimetableComponent },
+    { path: 'createuser', component: CreateUserComponent},
+    { path: 'deleteuser', component: DeleteUserComponent},
+    { path: 'login', component: LoginPageComponent},
+    { path: 'updateuser', component: UpdateUserComponent}
 ];
 
 @NgModule({
