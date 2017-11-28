2fc0a7a7569e51e2bd8cdb63065d3ff513753dda
Stefania <stefaniavlg@yahoo.com>
2017-11-28 10:34:16 +0200

    fiexed update-user.component.ts error that was "fixed"

--- GymAppUI/src/app/pages/user/update-user/update-user.component.ts
+++ GymAppUI/src/app/pages/user/update-user/update-user.component.ts
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import { Component, OnInit } from '@angular/core';
-import { User } from "../../../models/user";
-import { BackendService } from "../../../backend.service";
+import { User } from '../../../models/user';
+import { BackendService } from '../../../backend.service';
 import { SessionStorageService } from 'ngx-webstorage';
-import { Subscription } from "../../../models/subscriptionModel";
+import { Subscription } from '../../../models/subscriptionModel';
 
 @Component( {
     selector: 'app-update-user',
@@ -19,61 +19,19 @@ export class UpdateUserComponent implements OnInit {
 
     ngOnInit() {
         this.createdUser = this.session.retrieve( 'userToUpdate' );
-        
-        this.getAllSubs();
+this.getAllSubs();
     }
 
-    getAllSubs(){
+    getAllSubs() {
         this.backendService.getAllSubscriptionsForUser().subscribe( res => {
             this.subscriptionList = res;
         } );
     }
-    
     updateUser() {
-        debugger;
         this.backendService.addUser( this.createdUser ).subscribe( res => {
             this.message = res;
         } );
     }
 }
 
-=======
-import { Component, OnInit } from '@angular/core';
-import { User } from "../../../models/user";
-import { BackendService } from "../../../backend.service";
-import { SessionStorageService } from 'ngx-webstorage';
-import { Subscription } from "../../../models/subscriptionModel";
 
-@Component( {
-    selector: 'app-update-user',
-    templateUrl: './update-user.component.html',
-    styleUrls: ['./update-user.component.css']
-} )
-export class UpdateUserComponent implements OnInit {
-    createdUser: User;
-    message = '';
-
-    subscriptionList: Subscription[];
-    constructor( private backendService: BackendService, private session: SessionStorageService ) { }
-
-    ngOnInit() {
-        this.createdUser = this.session.retrieve( 'userToUpdate' );
-        
-        this.getAllSubs();
-    }
-
-    getAllSubs(){
-        this.backendService.getAllSubscriptionsForUser().subscribe( res => {
-            this.subscriptionList = res;
-        } );
-    }
-    
-    updateUser() {
-        debugger;
-        this.backendService.addUser( this.createdUser ).subscribe( res => {
-            this.message = res;
-        } );
-    }
-}
-
->>>>>>> a6852aae0df2766786816a2e3e8ccf2a63b94a1f
