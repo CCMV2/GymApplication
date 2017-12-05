67e3dbeaca44adcd86d5c8a8087f19e714e99a26
gellert.peter <geraldlionb@gmail.com>
2017-12-05 22:43:28 +0200

    TimeTable Stuff plus one tiny fix

--- GymAppUI/src/app/pages/user/user-timetable/user-timetable.component.ts
+++ GymAppUI/src/app/pages/user/user-timetable/user-timetable.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from "../../../models/user";
 import { BackendService } from '../../../backend.service';
 import {SessionStorageService} from 'ngx-webstorage';
+import { Timetable } from "../../../models/Timetable";
 @Component({
   selector: 'app-user-timetable',
   templateUrl: './user-timetable.component.html',
@@ -10,10 +11,20 @@ import {SessionStorageService} from 'ngx-webstorage';
 export class UserTimetableComponent implements OnInit {
 
     userClient : Client;
+    timeList : Timetable[];
   constructor( private backendService: BackendService,private session: SessionStorageService) { }
 
   ngOnInit() {
       this.userClient = this.session.retrieve('userToUpdate');
   }
+  
+  getAllTimetables() {
+      this.backendService.getAllTimetables().subscribe(res => {
+          this.timeList = res;
+          for (let timeElem of this.timeList) {
+              timeElem.timeTableName = timeElem.workoutType + ' ' + timeElem.start + ' ' + timeElem.duration;
+          }
+      });
+    }
 
 }
