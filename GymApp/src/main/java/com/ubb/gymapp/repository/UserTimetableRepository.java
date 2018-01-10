07fd2c68e59f910ae9700e9ad4c3d7425ac9c978
Ganditorul <robert_eu_97@yahoo.com>
2018-01-10 19:41:53 +0200

    Steluta cartonase (daca e subscribed)

--- GymApp/src/main/java/com/ubb/gymapp/repository/UserTimetableRepository.java
+++ GymApp/src/main/java/com/ubb/gymapp/repository/UserTimetableRepository.java
@@ -1,5 +1,7 @@
 package com.ubb.gymapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +18,7 @@ public interface UserTimetableRepository extends JpaRepository<UserTimetable, Lo
 	public void deleteByUser(User user);
 	
 	public UserTimetable findByUserAndTimetable(User user, Timetable timetable);
+
+	@Query("select u.timetable from UserTimetable u where u.user.email = ?1")
+	public List<Timetable> getUserTimetables(String username);
 }
